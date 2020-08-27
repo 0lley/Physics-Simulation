d2bd0b3484c0a207a77a15de6c26a3248c0271d0
0lley <Olexandr.Kurakin@gmail.com>
2020-08-27 22:36:39 +1000

    Added internal documentation and removed unnecessary code

--- p5/Project/bodies.js
+++ p5/Project/bodies.js
@@ -12,28 +12,26 @@ class Celestial {
     draw() {
         noStroke();
         circle(this.pos.x, this.pos.y, this.radius);
-    }
+    } // Displays the object
 
     updateEnt(inputPos, inputMass) {
-        // this.vel.add(this.mass*10000/pow((dist(baryCentre, this.pos)), 2));
         this.relX = inputPos.x - this.pos.x;
         this.relY = inputPos.y - this.pos.y;
-        this.magnitude = (bigG * inputMass) / Math.pow(dist(this.pos.x, this.pos.y, inputPos.x, inputPos.y) + 5, 2);
-        // console.log(inputMass);
+        this.magnitude = (document.getElementById("bigG").value / 10 * inputMass) / Math.pow(dist(this.pos.x, this.pos.y, inputPos.x, inputPos.y) + 5, 2); // This calculation determines the magnitude of the acceleration experienced by the object imparted by gravity
         if (this.relX >= 0 && this.relY <= 0) {
             this.heading = Math.atan(abs(this.relX)/abs(this.relY));
-        }
+        } // Solution if the object is southwest of the input entity
         else if (this.relX >= 0 && this.relY >= 0) {
             this.heading = Math.PI - Math.atan(abs(this.relX)/abs(this.relY));
-        }
+        } // Solution if the object is northwest of the input entity
         else if (this.relX <= 0 && this.relY >= 0) {
             this.heading = Math.atan(abs(this.relX)/abs(this.relY)) + Math.PI;
-        }
+        } // Solution if the object is northeast of the input entity
         else {
             this.heading = 2 * Math.PI - Math.atan(abs(this.relX)/abs(this.relY));
-        }
-        this.accel.set(this.magnitude * Math.sin(this.heading), this.magnitude * Math.cos(this.heading) * -1);
-        this.vel.add(this.accel);
-        this.pos.add(this.vel);
+        } // Solution if the object is southeast of the input entity
+        this.accel.set(this.magnitude * Math.sin(this.heading), this.magnitude * Math.cos(this.heading) * -1); // Sets the acceleration of the object, and uses trigonometric vector decomposition to accomplish this
+        this.vel.add(this.accel); // Changes the velocity of the object based on its acceleration
+        this.pos.add(this.vel); // Changes the position of the object based on its velocity
     }
 }
--- p5/Project/sketch.js
+++ p5/Project/sketch.js
@@ -8,35 +8,31 @@ function setup() {
   canvas.style("top", "0");
   background(5, 80);
   centre = createVector(windowWidth / 2, windowHeight * 0.8 / 2); // Location of parent star
-  starMass = document.getElementById("starMass").value
-  bigG = document.getElementById("bigG").value / 10;
 }
 
 function draw() {
   background(5, 80);
   noStroke();
-  circle(centre.x, centre.y, sqrt(starMass) * 0.15);
+  circle(centre.x, centre.y, sqrt(document.getElementById("starMass").value) * 0.15); // Drawing the centre star
   for (i = 0; i < entities.length; i++) {
       for(j = 0; j < entities.length; j++) {
           if (i != j) {
-            entities[i].updateEnt(entities[j].pos, entities[j].mass);
+            entities[i].updateEnt(entities[j].pos, entities[j].mass); // Loops through every object and uses Newtonian gravity to calculate the acceleration which must be imparted on the object, unless the object is itself
           }
       }
       if (entities[i].pos.x > windowWidth + 2500 || entities[i].pos.x < -2500 || entities[i].pos.y < -2500 || entities[i].pos.y > windowHeight + 2500) {
-        entities.splice(i, 1);
+        entities.splice(i, 1); // Removes the object if it goes too far away from the screen
       }
       else {
-        entities[i].updateEnt(centre, starMass);
-        entities[i].draw();
+        entities[i].updateEnt(centre, document.getElementById("starMass").value);
+        entities[i].draw(); // Updates and displays the object itself
       }
     }
-    starMass = document.getElementById("starMass").value
-    bigG = document.getElementById("bigG").value / 10;
 }
 
 function mouseClicked() {
     if (mouseX <= windowWidth && mouseX >= 0 && mouseY <= windowHeight * 0.8 && mouseY >= 0) {
-        entities.push(new Celestial());
+        entities.push(new Celestial()); // Creates a new object at the mouse if it is onscreen
     }
 }
 
